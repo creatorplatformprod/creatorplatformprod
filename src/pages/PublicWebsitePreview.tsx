ca10df801c65ca4fde32275422fc2a251b639eab
lannadelulu <lannadelulu@gmail.com>
2026-02-03 04:06:57 +0100

    Adjust preview frame container widths

--- src/pages/PublicWebsitePreview.tsx
+++ src/pages/PublicWebsitePreview.tsx
@@ -129,7 +129,7 @@ const PublicWebsitePreview = () => {
 
         <div className="flex flex-col lg:flex-row gap-10 items-start">
           <div className="flex-1 min-w-0 w-full">
-            <div className="relative mx-auto max-w-[920px]">
+            <div className="relative mx-auto max-w-[940px] lg:pr-4">
               <div className="bg-[#0c1119] rounded-[20px] p-3 border border-white/10 shadow-[0_30px_80px_rgba(2,6,23,0.6)]">
                 <div className="bg-black rounded-[14px] overflow-hidden border border-white/5">
                   <div className="relative mx-auto [--viewport-w:1440px] [--viewport-h:900px] [--scale:0.4] lg:[--scale:0.56] w-[calc(var(--viewport-w)*var(--scale))] h-[calc(var(--viewport-h)*var(--scale))]">
@@ -150,8 +150,8 @@ const PublicWebsitePreview = () => {
             </div>
           </div>
 
-          <div className="w-full lg:w-[380px] flex justify-center">
-            <div className="bg-[#0b0f16] rounded-[40px] p-4 shadow-[0_25px_70px_rgba(2,6,23,0.55)] border border-white/10 w-[340px]">
+          <div className="w-full lg:w-[400px] flex justify-center">
+            <div className="bg-[#0b0f16] rounded-[40px] p-3 shadow-[0_25px_70px_rgba(2,6,23,0.55)] border border-white/10 w-[352px]">
               <div className="bg-black rounded-[32px] overflow-hidden border border-white/5 relative">
                 <div className="absolute top-3 left-1/2 -translate-x-1/2 w-20 h-5 bg-[#0f1219] rounded-full border border-white/5 z-10" />
                 <div className="relative mx-auto [--viewport-w:430px] [--viewport-h:932px] [--scale:0.58] lg:[--scale:0.7] w-[calc(var(--viewport-w)*var(--scale))] h-[calc(var(--viewport-h)*var(--scale))]">
