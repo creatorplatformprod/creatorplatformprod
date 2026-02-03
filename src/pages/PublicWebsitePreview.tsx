594f37d1b64f6df2291be40335aaddfb989da8f4
lannadelulu <lannadelulu@gmail.com>
2026-02-03 05:15:25 +0100

    Slightly narrow mobile preview frame

--- src/pages/PublicWebsitePreview.tsx
+++ src/pages/PublicWebsitePreview.tsx
@@ -132,7 +132,7 @@ const PublicWebsitePreview = () => {
             <div className="relative mx-auto max-w-[920px]">
               <div className="bg-[#0c1119] rounded-[20px] p-3 border border-white/10 shadow-[0_30px_80px_rgba(2,6,23,0.6)]">
                 <div className="bg-black rounded-[14px] overflow-hidden border border-white/5">
-                  <div className="relative mx-auto [--viewport-w:1440px] [--viewport-h:900px] [--scale:0.34] sm:[--scale:0.38] lg:[--scale:0.56] w-[calc(var(--viewport-w)*var(--scale))] h-[calc(var(--viewport-h)*var(--scale))]">
+                  <div className="relative mx-auto [--viewport-w:1440px] [--viewport-h:900px] [--scale:0.32] sm:[--scale:0.36] lg:[--scale:0.56] w-[calc(var(--viewport-w)*var(--scale))] h-[calc(var(--viewport-h)*var(--scale))]">
                     <iframe
                       title="Desktop preview"
                       src={previewUrl}
@@ -151,7 +151,7 @@ const PublicWebsitePreview = () => {
           </div>
 
           <div className="w-full lg:w-[380px] flex justify-center">
-            <div className="bg-[#0b0f16] rounded-[40px] p-4 shadow-[0_25px_70px_rgba(2,6,23,0.55)] border border-white/10 w-[280px] sm:w-[320px] lg:w-[340px]">
+            <div className="bg-[#0b0f16] rounded-[40px] p-4 shadow-[0_25px_70px_rgba(2,6,23,0.55)] border border-white/10 w-[268px] sm:w-[310px] lg:w-[340px]">
               <div className="bg-black rounded-[32px] overflow-hidden border border-white/5 relative">
                 <div className="absolute top-3 left-1/2 -translate-x-1/2 w-20 h-5 bg-[#0f1219] rounded-full border border-white/5 z-10" />
                 <div className="relative mx-auto [--viewport-w:430px] [--viewport-h:932px] [--scale:0.54] sm:[--scale:0.58] lg:[--scale:0.7] w-[calc(var(--viewport-w)*var(--scale))] h-[calc(var(--viewport-h)*var(--scale))]">
