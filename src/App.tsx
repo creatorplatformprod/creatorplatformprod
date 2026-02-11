1e54dc7d8275945c3ddad9817d858e8646a07a50
lannadelulu <lannadelulu@gmail.com>
2026-02-11 05:55:02 +0100

    Force app theme provider to light

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute="class" defaultTheme="dark" forcedTheme="dark" enableSystem={false}>
+    <ThemeProvider attribute="class" defaultTheme="light" forcedTheme="light" enableSystem={false}>
       <TooltipProvider>
         <Toaster />
         <Sonner />
